06cdead4bdba226ef957534c3f01c1c23435c8a6
Kayleb O'Brien <1003562@student.sae.edu.au>
2017-04-05 04:29:19 +1000

    Phase Control and Test Scene w/ basic Phase Window
    Phase control with camera script along with basic, conceptual phase preview window in test scene for demo.
    
    Unfortunate automatic removal of empty folders.

---
+++ Assets/Scripts/TestScripts/PhaseScript.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhaseScript : MonoBehaviour {
+
+    [Header("Unit Offset Between Worlds")]
+    public float worldOffset;
+
+    [Header("Search For Objects by Tag?")]
+    public bool tagSearch = false;
+
+    [Header("Objects")]
+    public GameObject player;
+    public GameObject phaseCamera;
+
+    [Header("Player Above or Below?")]
+    public bool playerAbove = true;
+
+    [Header("Phase Camera Offset")]
+    public float phaseCameraOffset;
+
+    void Start ()
+    {
+        if (tagSearch == true)
+        {
+            SearchForObjects();
+        }
+
+        if (playerAbove)
+        {
+            phaseCameraOffset = -worldOffset;
+        }
+        else if (!playerAbove)
+        {
+            phaseCameraOffset = worldOffset;
+        }
+    }
+	
+	void Update ()
+    {
+        PhaseCameraControl();
+
+        Transition();
+	}
+
+    void Transition()
+    {
+        if (Input.GetKeyDown("e"))
+        {
+            if (playerAbove)
+            {
+                player.transform.position = new Vector3(player.transform.position.x,
+                                                        player.transform.position.y - worldOffset,
+                                                        player.transform.position.z);
+                phaseCameraOffset = worldOffset;
+                playerAbove = false;
+            }
+            else if (!playerAbove)
+            {
+                player.transform.position = new Vector3(player.transform.position.x,
+                                                        player.transform.position.y + worldOffset,
+                                                        player.transform.position.z);
+                phaseCameraOffset = -worldOffset;
+                playerAbove = true;
+            }
+        }
+    }
+
+    void SearchForObjects()
+    {
+        player      = GameObject.FindGameObjectWithTag("Player");
+        phaseCamera = GameObject.FindGameObjectWithTag("PhaseCamera");
+    }
+
+    void PhaseCameraControl()
+    {
+        Vector3 targetPosition = new Vector3(player.transform.position.x,
+                                             player.transform.position.y + phaseCameraOffset,
+                                             player.transform.position.z);
+
+        phaseCamera.transform.position = targetPosition;
+        phaseCamera.transform.rotation = player.transform.rotation;
+    }
+}
---
+++ Assets/Scripts/TestScripts/Spin.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour {
+
+    public float rotateSpeed = 1;
+
+	void Update () {
+        transform.Rotate(Vector3.up * Time.deltaTime * rotateSpeed);
+    }
+}
