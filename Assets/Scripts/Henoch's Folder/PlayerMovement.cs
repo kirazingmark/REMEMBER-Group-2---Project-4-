3301c3ac529a4e157f9c54e3f061ccbd67be6dae
Henoch Yanuary <1003495@student.sae.edu.au>
2017-04-11 22:25:16 +0900

    updating the analytical for timer

--- Assets/Scripts/Henoch's Folder/PlayerMovement.cs
+++ Assets/Scripts/Henoch's Folder/PlayerMovement.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     public float movementSpeed;
+	public float jumpingForce;
+	public float groundDist;
+
 
 	// Use this for initialization
 	void Start () {
-		
+		groundDist = this.transform.GetComponent<Collider>().bounds.extents.y;
+
 	}
 	
 	// Update is called once per frame
@@ -26,6 +30,18 @@ public class PlayerMovement : MonoBehaviour {
         //move front to back
         float vertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
         transform.Translate(0, 0, vertical);
-    }
+
+		//jumping part
+		if(Input.GetKey(KeyCode.Space) && CheckGrounded () == true )
+		{
+			
+			this.transform.GetComponent<Rigidbody>().AddForce(0, jumpingForce, 0);
+		}
+	}
+
+	public bool CheckGrounded()
+	{
+		return Physics.Raycast(this.transform.position, -Vector3.up, groundDist + 0.1f);
+	}
 
 }
---
+++ Assets/Scripts/Henoch's Folder/UIManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+
+    public Text time;
+    public Text timeBig;
+    public float timer = 0.0f;
+    public float pTimer;
+    public bool timerCheck = true;
+    public bool deleteData;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (deleteData)
+            PlayerPrefs.DeleteAll();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerCheck != false)
+        {
+            timer += Time.deltaTime;
+            pTimer += Time.deltaTime;
+            time.text = "Current TIme = " + timer.ToString("F1");
+
+        }
+        timeBig.text = "Previous TIme = " + PlayerPrefs.GetFloat("timer").ToString("F1");
+
+    }
+
+    void OnApplicationQuit()
+    {
+        timerCheck = false;
+        PlayerPrefs.SetFloat("timer", pTimer);
+        PlayerPrefs.Save();
+    }
+}
