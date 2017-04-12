2aac7b4b20a778ecd17fd14ce911c58ff33381e9
SAE\hyanuary <1003495@student.sae.edu.au>
2017-04-12 18:28:59 +1000

    updating the ui manager, make the timer in minutes and seconds, save the run time of previous play

--- Assets/Scripts/Henoch's Folder/UIManager.cs
+++ Assets/Scripts/Henoch's Folder/UIManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
-
+using System.IO;
 public class UIManager : MonoBehaviour
 {
 
@@ -10,8 +10,13 @@ public class UIManager : MonoBehaviour
     public Text timeBig;
     public float timer = 0.0f;
     public float pTimer;
+    public float cTimer;
+    public float minute;
+    public float pMinute;
     public bool timerCheck = true;
     public bool deleteData;
+
+    List<float> fList = new List<float>();
 
     // Use this for initialization
     void Start()
@@ -23,14 +28,26 @@ public class UIManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if (timerCheck != false)
         {
             timer += Time.deltaTime;
             pTimer += Time.deltaTime;
-            time.text = "Current TIme = " + timer.ToString("F1");
+            cTimer += Time.deltaTime;
+            time.text = "Time = " + minute.ToString() + "m " + timer.ToString("F1") + "s";
 
         }
-        timeBig.text = "Previous TIme = " + PlayerPrefs.GetFloat("timer").ToString("F1");
+        if (timer > 60)
+        {
+            timer = 0;
+            minute += 1;
+        }
+        if (cTimer > 60)
+        {
+            cTimer = 0;
+            pMinute += 1;
+        }
+        timeBig.text = "Best Time = " + PlayerPrefs.GetFloat("minute").ToString("F0") + "m " + PlayerPrefs.GetFloat("cTimer").ToString("F1") + "s";
 
     }
 
@@ -38,6 +55,21 @@ public class UIManager : MonoBehaviour
     {
         timerCheck = false;
         PlayerPrefs.SetFloat("timer", pTimer);
+        PlayerPrefs.SetFloat("cTimer", cTimer);
+        PlayerPrefs.SetFloat("minute", pMinute);
         PlayerPrefs.Save();
+        //saving the time to the list
+        fList.Add(cTimer);
+        //converting from float list to string list
+        List<string> sList = fList.ConvertAll<string>(x => x.ToString());
+        //converting from string list to string
+        string myString = string.Join(",", sList.ToArray());
+        //writing the timer to a folder
+        StreamWriter sw = File.AppendText("TextFolder");
+        //myString = "thing";
+        sw.WriteLine(myString);
+        sw.Close();
+
+        // System.IO.File.WriteAllText ("TextFolder", myString);
     }
 }
