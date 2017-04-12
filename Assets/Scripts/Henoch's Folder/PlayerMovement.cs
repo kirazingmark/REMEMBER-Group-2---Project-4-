f51964650c5a4696a9bec427aee4bd2fa988b2d0
SAE\hyanuary <1003495@student.sae.edu.au>
2017-04-12 19:04:08 +1000

    updating the player movement by using addForce

--- Assets/Scripts/Henoch's Folder/PlayerMovement.cs
+++ Assets/Scripts/Henoch's Folder/PlayerMovement.cs
@@ -24,20 +24,29 @@ public class PlayerMovement : MonoBehaviour {
     void Movement()
     {
         //move left to right
-        float horizontal = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
-        transform.Translate(horizontal, 0, 0);
+        //float horizontal = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.W))
+            rb.AddForce(transform.forward * movementSpeed);
+        else if (Input.GetKey(KeyCode.S))
+            rb.AddForce(transform.forward * -movementSpeed);
 
         //move front to back
-        float vertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
-        transform.Translate(0, 0, vertical);
+        // float vertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.D))
+            rb.AddForce(transform.right * movementSpeed);
+        else if (Input.GetKey(KeyCode.A))
+            rb.AddForce(transform.right * -movementSpeed);
 
-		//jumping part
-		if(Input.GetKey(KeyCode.Space) && CheckGrounded () == true )
-		{
-			
-			this.transform.GetComponent<Rigidbody>().AddForce(0, jumpingForce, 0);
-		}
-	}
+        if (rb.velocity.magnitude > movementSpeed)
+            rb.velocity = rb.velocity.normalized * movementSpeed;
+
+        //jumping part
+        if (Input.GetKey(KeyCode.Space) && CheckGrounded() == true)
+        {
+
+            rb.AddForce(0, jumpingForce, 0);
+        }
+    }
 
 	public bool CheckGrounded()
 	{
