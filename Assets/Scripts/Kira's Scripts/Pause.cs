760a8b296a73cffc1bda3994f1c0d1ef4ff5b8c8
Kira Zingmark <kirazingmark@gmail.com>
2017-04-25 18:50:34 +1000

    Pause Menu Overlay Updated.
    - Pause Script implemented temporarily (will merge into PlayerController in next Commit).
    - Pause Panel added to Canvas in 'Mel's Scene 2'.

---
+++ Assets/Scripts/Kira's Scripts/Pause.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour {
+
+    // VARIABLES AND CONSTANTS.
+    public GameObject pausePanel;
+    public AudioSource backgroundMusic;
+    public AudioSource environmentalSound1;
+    public AudioSource environmentalSound2;
+
+
+    // Use this for initialization
+    public void Start () {
+
+        pausePanel.SetActive(false);
+        backgroundMusic.Play();
+        Time.timeScale = 1;
+    }
+
+    public void OnPause() {
+
+        pausePanel.SetActive(true);
+        backgroundMusic.Pause();
+        Time.timeScale = 0;
+    }
+
+    public void OnUnPause() {
+
+        pausePanel.SetActive(false);
+        backgroundMusic.Play();
+        Time.timeScale = 1;
+    }
+}
